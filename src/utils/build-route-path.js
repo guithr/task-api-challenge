bfcd5581d581b77f73b4da92e5c98f8c4bd968fa
guithr <guilherme.thrmatos@gmail.com>
2025-02-28 13:26:28 -0300

    feat: finalize task CRUD API with CSV import functionality

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,12 @@
+//  path: "/tasks/:id",
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-z]+)/g;
+  const pathWithParams = path.replaceAll(
+    routeParametersRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+
+  return pathRegex;
+}
